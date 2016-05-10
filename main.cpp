e48f6a69318042dc667989ddff56c5699d732cf2
Jeff T <jeffreystalbot@gmail.com>
2016-05-10 13:54:55 -0400

    Added code that translates Hi to Hello

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-/* 
- * File:   main.cpp
- * Author: Jared
- *
- * Created on May 10, 2016, 1:30 PM
- */
-
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -13,7 +8,18 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
-    return 1337;
+    ifstream input;
+    input.open("input.txt");
+    ofstream output;
+    output.open("output.txt");
+    char newLine[100];
+    int loop = 0;
+    while(input.get(newLine[loop])){
+        cout << newLine[loop];
+        loop++;
+    }
+    if(newLine[0] == 'H' and newLine[1] == 'i'){
+        output << "Hello";
+    }
+    return 0;
 }
-
