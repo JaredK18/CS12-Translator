66ccd1c27399f95e7ff76d6ff23c379bc736e673
Jeff T <jeffreystalbot@gmail.com>
2016-05-26 14:19:33 -0400

    FULLY MODUMALIZED

--- main.cpp
+++ main.cpp
@@ -18,30 +18,24 @@ using namespace std;
 bool stringMatch(char fileContents[][1000], int rowIndex, int startPosition, string searchTerm);
 int variableDeclaration(char fileContents[][1000], int rowIndex, int columnIndex);
 void consoleOutput(char fileContents[][1000], int& rowIndex, int& columnIndex, ofstream &outputPy, ofstream &outputTxt);
+void ifStatement(char fileContents[][1000], int& rowIndex, int& columnIndex, ofstream &outputPy, ofstream &outputTxt);
 template <typename genericOutput> void fileOutput(genericOutput output, ofstream &outputPy, ofstream &outputTxt){
     outputPy << output;
     outputTxt << output;
 }
-//void pyFileCreator(char )
-//void print(string maybe);
 
 int main(int argc, char** argv){
     ofstream outputPy;
-    outputPy.open("outputpy.py", ios::app);
-    ofstream outputTxt;
-    outputTxt.open("outputtxt.txt", ios::app);
-    ifstream input;
-    input.open("input.txt");
-    /*ofstream outputPy;
     outputPy.open("outputpy.py");
     ofstream outputTxt;
-    outputTxt.open("outputtxt.txt");*/
+    outputTxt.open("outputtxt.txt");
+    ifstream input;
+    input.open("input.txt");
     char fileContents[1000][1000];
     int rowIndex = 0;
     int columnIndex = 0;
     int lastChar = 0;
     int lastLine = 0;
-    int tabs = 0;
     bool beforeCode = false;
     bool doPrint = true;
     while(input.get(fileContents[lastLine][lastChar])){
@@ -66,77 +60,15 @@ int main(int argc, char** argv){
         if(fileContents[rowIndex][columnIndex] == ';'){
             columnIndex++;
         }
-		//Process outputs to the console
+	//Process outputs to the console
         else if(stringMatch(fileContents, rowIndex, columnIndex, "cout")){
             consoleOutput(fileContents, rowIndex, columnIndex, outputPy, outputTxt);
-            /*columnIndex = columnIndex + 4;
-            fileOutput("print (");
-            while(fileContents[rowIndex][columnIndex] == ' ' or fileContents[rowIndex][columnIndex] == '<'){
-                columnIndex++;
-            }
-            while(fileContents[rowIndex][columnIndex] != ';'){
-                if(fileContents[rowIndex][columnIndex] == '"'){
-                    inQuotes = true;
-                }
-                while(inQuotes){
-                    fileOutput(fileContents[rowIndex][columnIndex]);
-                    columnIndex++;
-                    if(fileContents[rowIndex][columnIndex] == '"'){
-                        inQuotes = false;
-                    }
-                }
-                if(stringMatch(fileContents, rowIndex, columnIndex, "<<")){
-                    columnIndex = columnIndex + 2;
-                    fileOutput(fileContents[rowIndex][columnIndex]);
-                }
-                else if(fileContents[rowIndex][columnIndex] == ' '){
-                    columnIndex++;
-                }
-                else if(fileContents[rowIndex][columnIndex] == '\n'){                    
-                    fileOutput(")\n");
-                    rowIndex++;
-                    columnIndex = 0;    
-                    fileOutput("print (");
-                }
-                else{
-                    fileOutput(fileContents[rowIndex][columnIndex]);
-                    columnIndex++;
-                }
-            }
-            columnIndex++;
-            fileOutput(")");*/
         }
-        else if(stringMatch(fileContents, rowIndex, columnIndex, "if(")){
-            fileOutput("if ", outputPy, outputTxt);
-            columnIndex = columnIndex + 3;
-            tabs++;
-            while(fileContents[rowIndex][columnIndex] != ')'){
-                fileOutput(fileContents[rowIndex][columnIndex], outputPy, outputTxt);
-                columnIndex++;
-            }
-            columnIndex = columnIndex + 2;
-            fileOutput(":", outputPy, outputTxt);
-        }
-        else if(stringMatch(fileContents, rowIndex, columnIndex, "else if(")){
-            fileOutput("elif ", outputPy, outputTxt);
-            columnIndex = columnIndex + 8;
-            tabs++;
-            while(fileContents[rowIndex][columnIndex] != ')'){
-                fileOutput(fileContents[rowIndex][columnIndex], outputPy, outputTxt);
-                columnIndex++;
-            }
-            columnIndex = columnIndex + 2;
-            fileOutput(":", outputPy, outputTxt);
-        }
-        else if(stringMatch(fileContents, rowIndex, columnIndex, "else{")){
-            fileOutput("else:", outputPy, outputTxt);
-            columnIndex = columnIndex + 5;
-            tabs++;
-        }
-        else if(fileContents[rowIndex][columnIndex] == '}'){
+        //Process if/else/else if
+        ifStatement(fileContents, rowIndex, columnIndex, outputPy, outputTxt);
+        if(fileContents[rowIndex][columnIndex] == '}'){
             columnIndex++;
             doPrint = false;
-            tabs--;
         }
         if(doPrint){
             fileOutput(fileContents[rowIndex][columnIndex], outputPy, outputTxt);
@@ -226,4 +158,35 @@ void consoleOutput(char fileContents[][1000], int& rowIndex, int& columnIndex, o
     }
     columnIndex++;
     fileOutput(")", outputPy, outputTxt);
+    return;
+}
+
+void ifStatement(char fileContents[][1000], int& rowIndex, int& columnIndex, ofstream &outputPy, ofstream &outputTxt){
+    if(stringMatch(fileContents, rowIndex, columnIndex, "if(")){
+        fileOutput("if ", outputPy, outputTxt);
+        columnIndex = columnIndex + 3;
+        while(fileContents[rowIndex][columnIndex] != ')'){
+            fileOutput(fileContents[rowIndex][columnIndex], outputPy, outputTxt);
+            columnIndex++;
+        }
+        columnIndex = columnIndex + 2;
+        fileOutput(":", outputPy, outputTxt);
+        return;
+    }
+    else if(stringMatch(fileContents, rowIndex, columnIndex, "else if(")){
+        fileOutput("elif ", outputPy, outputTxt);
+        columnIndex = columnIndex + 8;
+        while(fileContents[rowIndex][columnIndex] != ')'){
+            fileOutput(fileContents[rowIndex][columnIndex], outputPy, outputTxt);
+            columnIndex++;
+        }
+        columnIndex = columnIndex + 2;
+        fileOutput(":", outputPy, outputTxt);
+        return;
+    }
+    else if(stringMatch(fileContents, rowIndex, columnIndex, "else{")){
+        fileOutput("else:", outputPy, outputTxt);
+        columnIndex = columnIndex + 5;
+        return;
+    }
 }
